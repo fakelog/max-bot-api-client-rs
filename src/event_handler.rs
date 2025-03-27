9304dab0881156826a618cbc98940452337ee9f0
fakelog <fakelog@users.noreply.github.com>
2025-03-27 19:23:47 +0500

    add longpoll

---
+++ src/event_handler.rs
@@ -0,0 +1,40 @@
+use tokio::sync::mpsc;
+
+use crate::{client::MaxBotClient, models::Update, types::Result};
+
+pub struct EventHandler {
+    client: MaxBotClient,
+    marker: Option<i64>,
+    event_sender: mpsc::Sender<Update>,
+}
+
+impl EventHandler {
+    pub fn new(client: MaxBotClient) -> (Self, mpsc::Receiver<Update>) {
+        let (sender, receiver) = mpsc::channel(100);
+        (
+            Self {
+                client,
+                marker: None,
+                event_sender: sender,
+            },
+            receiver,
+        )
+    }
+
+    pub async fn start(&mut self) -> Result<()> {
+        loop {
+            let updates = self
+                .client
+                .get_updates(Some(100), Some(30), self.marker, None)
+                .await?;
+
+            // Отправляем все обновления через канал
+            for update in updates.updates {
+                self.event_sender.send(update).await?;
+            }
+
+            // Обновляем маркер для следующего запроса
+            self.marker = updates.marker;
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod client;
 pub mod error;
+pub mod event_handler;
 pub mod models;
 pub mod types;
