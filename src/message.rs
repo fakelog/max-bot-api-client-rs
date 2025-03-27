0bbf0879bda5750ef0be4c534d9e2f8c36f40a3d
fakelog <fakelog@users.noreply.github.com>
2025-03-28 01:13:06 +0500

    add edit function

--- src/message.rs
+++ src/message.rs
@@ -1,7 +1,9 @@
 use crate::{
-    api,
-    api::models::{Message, MessageLinkType, NewMessageBody, NewMessageLink, SendMessageResult},
-    client::MaxBotClient,
+    api::{
+        self,
+        models::{Message, MessageLinkType, NewMessageBody, NewMessageLink, SendMessageResult},
+    },
+    client::{self, MaxBotClient},
 };
 
 type Result = api::types::Result<SendMessageResult>;
@@ -60,5 +62,22 @@ impl MessageExt for Message {
             .api_client
             .send_message(&message, self.recipient.chat_id, None)
             .await
+    }
+}
+
+pub trait SendMessageResultExt {
+    fn edit(
+        &self,
+        client: MaxBotClient,
+        text: &str,
+    ) -> impl std::future::Future<Output = ()> + Send;
+}
+
+impl SendMessageResultExt for SendMessageResult {
+    async fn edit(&self, client: MaxBotClient, text: &str) {
+        let message = NewMessageBody::new(text.into());
+        let message_id = self.message.body.mid.clone();
+
+        let _ = client.api_client.edit_message(&message_id, &message).await;
     }
 }
